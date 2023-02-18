8f0867409729a83f4a66985e8b5bbe2bae426cd0
cjmadanguit@gmail.com <cjmadanguit@gmail.com>
2023-02-18 05:42:31 +0000

    dbt test and commit

--- models/staging/stg_green_tripdata.sql
+++ models/staging/stg_green_tripdata.sql
@@ -1,5 +1,13 @@
 {{ config(materialized='view') }}
 
+with tripdata as 
+(
+  select *,
+    row_number() over(partition by vendorid, lpep_pickup_datetime) as rn
+  from {{ source('staging','green_taxi') }}
+where vendorid is not null 
+)
+
 Select 
 -- identifiers
     {{ dbt_utils.surrogate_key(['vendorid', 'lpep_pickup_datetime']) }} as tripid,
@@ -30,8 +38,8 @@ Select
     cast(payment_type as integer) as payment_type,
     {{ get_payment_type_description('payment_type') }} as payment_type_description, 
     cast(congestion_surcharge as numeric) as congestion_surcharge
-FROM {{ source('staging','green_taxi') }}
-where vendorid is not null 
+FROM tripdata
+where rn=1
 
 -- dbt build --m <model.sql> --var 'is_test_run: false'
 {% if var('is_test_run', default=true) %}
